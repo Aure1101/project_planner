7b9882d59eafcbd1301315936711269d940f2570
Aure1101 <aurelio_flores11@hotmail.com>
2024-04-22 19:50:27 -0600

    funciones de agregar y leer añadidas

---
+++ data_manager.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os.path
+
+if not os.path.exists('actividades.csv'):
+    act = pd.DataFrame({'nombre_actividad':[],
+                        'responsable': [],
+                        'fecha_inicio': [],
+                        'duracion': [],
+                        'porcentaje_terminado': []})
+    act.to_csv('actividades.csv', index=False)
+else:
+    act = pd.read_csv('actividades.csv')
+
+
+def create_act(act, nombre, responsable, fecha_inicio, duracion):
+    print(type(act))
+    row = pd.DataFrame({'nombre_actividad': [nombre], 'responsable': [responsable], 'fecha_inicio': [fecha_inicio], 'duracion': [duracion], 'porcentaje_terminado': [0]})
+    act = pd.concat([act,row])
+    act.to_csv('actividades.csv', index=False)
+
+def delete_act():
+    pass
+
+def update_completition():
+    pass
+
+def get_act():
+    return act.to_numpy()
