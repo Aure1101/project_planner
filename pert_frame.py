334243961c0f289949c908a389fd82fd47dd4213
RPM <odstrob@outool.com>
2024-05-24 13:28:42 -0600

    Avance en Calculos

--- pert_frame.py
+++ pert_frame.py
@@ -1,26 +1,56 @@
-import data_manager as dm # Imprtar el archivo data_manager
-import numpy as np # Importar la biblioteca numpy
+import data_manager as dm 
+import numpy as np
 from scipy.stats import norm
 
-actividades = dm.get_activities_gantt() # Instanciar el objeto para las actividades
+def calcular_varianza(actividades):
+    varianzas = []
+    for actividad in actividades:
+        TO = actividad[3]  # tiempo_optimista
+        TP = actividad[5]  # tiempo_pesimista
+        varianza = ((TP - TO) / 6) ** 2
+        varianzas.append(varianza)
+    varianza_total = np.sum(varianzas)
+    return varianza_total
 
-def calcular_varianza(actividades): # Metodo para calcular la varianza 
-    varianzas = [] # Arreglo para almacenar los datos 
-    for actividad in actividades: # Iteracion de cada fila de las actividades 
-        TO = actividad['tiempo_optimista'] # Guardar en una variable el tiempo Optimista 
-        TP = actividad['tiempo_pesimista'] # Guardar en una variable el tiempo pesimista 
-        varianza = ((TP - TO) / 6) ** 2 # Calculo de la varianza 
-        varianzas.append(varianza) # Agregar al calculo al arreglo
-    varianza_total = np.sum(varianzas) # SUmar los datos del arreglo
-    return varianza_total # Retornar el calculo
+def probar_calculos():
+    # Obtener las actividades desde el data_manager
+    actividades = dm.get_activities_gantt()
 
-def calcular_tiempo_esperado_total(actividades):
-    tiempo_esperado_total = actividades['tiempo_esperado'].sum()
-    return tiempo_esperado_total
+    # Calcular la varianza
+    varianza_total = calcular_varianza(actividades)
 
-def calcular_probabilidad(tiempo_esperado_total, varianza_total, fecha_limite):
+    # Imprimir el resultado
+    print("Varianza total:", varianza_total)
+
+'''
+def calcular_heuristica(actividades):
+    # Calculamos los tiempos de inicio más temprano (TIT), tiempos finales más tempranos (TFT),
+    # tiempos de inicio más tardíos (TAT) y tiempos finales más tardíos (TFT) para cada actividad.
+    TIT = np.zeros(len(actividades))
+    TFT = np.zeros(len(actividades))
+    TAT = np.zeros(len(actividades))
+    TTT = np.zeros(len(actividades)) # Tiempo de termino temprano
+    TIT[0] = 0
+    for i in range(len(actividades)):
+        TFT[i] = TIT[i] + actividades[i]['tiempo_esperado']
+        for dependencia in actividades[i]['dependencias']:
+            if TFT[i] > TIT[int(dependencia)]:
+                TIT[int(dependencia)] = TFT[i]
+    for i in range(len(actividades)-1, -1, -1):
+        TAT[i] = TTT[i] - TFT[i]
+        for dependiente in actividades[i]['dependientes']:
+            if TTT[int(dependiente)] == 0:
+                TTT[int(dependiente)] = TFT[i]
+            elif TTT[int(dependiente)] > TFT[i]:
+                TTT[int(dependiente)] = TFT[i]
+    heuristica = TTT - TIT
+    return heuristica
+
+def calcular_probabilidad(varianza_total, fecha_limite, tiempo_esperado_total):
     desviacion_estandar_total = np.sqrt(varianza_total)
     Z = (fecha_limite - tiempo_esperado_total) / desviacion_estandar_total
     probabilidad = norm.cdf(Z)
-    return probabilidad
- 
+    return probabilidad'''
+
+# Llamar a la función de prueba
+probar_calculos()
