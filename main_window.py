5c698bc55c6c05c8c967f239a590738147bd1df3
BetoPoli <odstrob@outlook.com>
2024-05-28 19:56:16 -0600

    Editor de actividades independiente

--- main_window.py
+++ main_window.py
@@ -2,23 +2,19 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from gantt_view import Gantt
 import activity_creator as ac
-#import activity_editor as ae
-import editor_actividad as ea
+from editor_actividad import open_selector
 
 window = tk.Tk()
+
 menu = tk.Frame(window)
 display = tk.Frame(window)
 
 gantt = Gantt(display)
 
-add_button = tk.Button(menu, text = 'Añadir Actividad', command=lambda :ac.open_(gantt))
-# Editar actividad
-#edit_button = tk.Button(menu, text = 'Editar Actividad', command=lambda:ae.open_(gantt))
-edit_button = tk.Button(menu, text = 'Editar Actividad', command=lambda: ea.open_(gantt))
-
-#En lugar de estos botones podriamos usar pestañas para cambiar entre las vistas
-gauss_button = tk.Button(menu, text = 'Visualizar\nDiagrama de Gantt')
-pert_button = tk.Button(menu, text = 'Visualizar\nPERT-CPM')
+add_button = tk.Button(menu, text='Añadir Actividad', command=lambda: ac.open_(gantt))
+edit_button = tk.Button(menu, text='Editar Actividad', command=lambda: open_selector(gantt))
+gauss_button = tk.Button(menu, text='Visualizar\nDiagrama de Gantt')
+pert_button = tk.Button(menu, text='Visualizar\nPERT-CPM')
 
 menu.grid(column=1, row=0)
 display.grid(column=0, row=0, sticky='NSEW')
@@ -34,10 +30,9 @@ menu.columnconfigure(0, weight=1)
 menu.columnconfigure(1, weight=1)
 menu.rowconfigure(0, weight=1)
 menu.rowconfigure(1, weight=1)
+menu.rowconfigure(2, weight=1)
 
 display.rowconfigure(0, weight=1)
 display.columnconfigure(0, weight=1)
 
 window.mainloop()
-
-
